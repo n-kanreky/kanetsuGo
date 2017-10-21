c0e36476ee2274762ecb1670d6c3c2542f136538
クロス尚美 <nfc_mac@kurosunaominoiMac.local>
2017-10-21 17:20:40 +0900

    Initial Commit

---
+++ KanetsuGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KanetsuGo
+//
+//  Created by クロス尚美 on 2017/10/21.
+//  Copyright © 2017年 NC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
