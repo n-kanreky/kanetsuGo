f1ab7c3db044f2a9b65c4563b956c0bafbc13f3d
n_kanreky <nfc_mac@kurosunaominoiMac.local>
2018-11-14 00:33:35 +0900

    PressStart その１）

---
+++ KanetsuGo/PressStartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PressStartViewController.swift
+//  KanetsuGo
+//
+//  Created by クロス尚美 on 2018/11/14.
+//  Copyright © 2018年 NC. All rights reserved.
+//
+
+import UIKit
+
+class PressStartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
